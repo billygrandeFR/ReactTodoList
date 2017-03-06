3d3b70482751100331cd524a521359ef92467b21
BillyGrande <kevinsheridancv@gmail.com>
2017-03-06 15:32:16 +0100

    toggle added

--- public/bundle.js
+++ public/bundle.js
@@ -26614,10 +26614,12 @@
 	      searchText: '',
 	      todos: [{
 	        id: uuid(),
-	        text: 'Walk the dog'
+	        text: 'Walk the dog',
+	        completed: false
 	      }, {
 	        id: uuid(),
-	        text: 'Clean the yard'
+	        text: 'Clean the yard',
+	        completed: true
 	      }]
 	    };
 	  },
@@ -26626,8 +26628,20 @@
 	    this.setState({
 	      todos: [].concat(_toConsumableArray(this.state.todos), [{
 	        id: uuid(),
-	        text: text
+	        text: text,
+	        completed: false
 	      }])
+	    });
+	  },
+	  handleToggle: function handleToggle(id) {
+	    var updatedTodos = this.state.todos.map(function (todo) {
+	      if (todo.id === id) {
+	        todo.completed = !todo.completed;
+	      }
+	      return todo;
+	    });
+	    this.setState({
+	      todos: updatedTodos
 	    });
 	  },
 	  handleSearch: function handleSearch(showCompleted, searchText) {
@@ -26644,7 +26658,7 @@
 	      'div',
 	      null,
 	      React.createElement(TodoSearch, { onSearch: this.handleSearch }),
-	      React.createElement(TodoList, { todos: todos }),
+	      React.createElement(TodoList, { todos: todos, onToggle: this.handleToggle }),
 	      React.createElement(AddTodo, { onTodo: this.handleAddTodo })
 	    );
 	  }
@@ -26668,11 +26682,13 @@
 	  displayName: 'TodoList',
 
 	  render: function render() {
+	    var _this = this;
+
 	    var todos = this.props.todos;
 
 	    var renderTodos = function renderTodos() {
 	      return todos.map(function (todo) {
-	        return React.createElement(Todo, _extends({ key: todo.id }, todo));
+	        return React.createElement(Todo, _extends({ key: todo.id }, todo, { onToggle: _this.props.onToggle }));
 	      });
 	    };
 
@@ -26690,23 +26706,27 @@
 /* 242 */
 /***/ function(module, exports, __webpack_require__) {
 
-	'use strict';
+	"use strict";
 
 	var React = __webpack_require__(8);
 
 	var Todo = React.createClass({
-	  displayName: 'Todo',
+	  displayName: "Todo",
 
 	  render: function render() {
+	    var _this = this;
+
 	    var _props = this.props,
 	        text = _props.text,
-	        id = _props.id;
+	        id = _props.id,
+	        completed = _props.completed;
 
 	    return React.createElement(
-	      'div',
-	      null,
-	      id,
-	      ': ',
+	      "div",
+	      { onClick: function onClick() {
+	          _this.props.onToggle(id);
+	        } },
+	      React.createElement("input", { type: "checkbox", checked: completed }),
 	      text
 	    );
 	  }
