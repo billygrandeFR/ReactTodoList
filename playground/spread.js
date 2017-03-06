259e561a3cc504f99bb138dee42b0bedcf94825f
BillyGrande <kevinsheridancv@gmail.com>
2017-03-06 14:32:40 +0100

    add playground for spread operator

---
+++ playground/spread.js
@@ -0,0 +1,20 @@
+var groupA = ['mathew', 'hakim', 'benoit'];
+var groupB = ['Aurelie', 'Carole', 'Valerie'];
+
+var mast = [...groupA, ...groupB];
+
+console.log(mast);
+
+var person1 = ['Andrew', 25];
+var person2 = ['Benoit', 36];
+
+function whatever(name, age){
+  return console.log('Hi ' + name + ' your age is ' + age);
+}
+whatever(...person2);
+
+var final = [...groupA, ...groupB];
+
+  final.forEach(function (name) {
+    console.log('Hi ' + name);
+  });
